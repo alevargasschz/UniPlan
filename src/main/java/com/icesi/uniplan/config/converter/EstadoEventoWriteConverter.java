e065ecbabca91ef0b105f1a44eafb7c923c45c13
Juan José <jjrr2331@gmail.com>
2026-05-24 16:37:47 -0500

    Annotate EstadoEventoWriteConverter with WritingConverter

--- src/main/java/com/icesi/uniplan/config/converter/EstadoEventoWriteConverter.java
+++ src/main/java/com/icesi/uniplan/config/converter/EstadoEventoWriteConverter.java
@@ -2,7 +2,9 @@ package com.icesi.uniplan.config.converter;
 
 import com.icesi.uniplan.model.mongo.enums.EstadoEvento;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.WritingConverter;
 
+@WritingConverter
 public class EstadoEventoWriteConverter implements Converter<EstadoEvento, String> {
 
     @Override
