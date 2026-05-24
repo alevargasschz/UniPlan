b4d640586d34c3231598ad3cc95914cd98d1babc
Juan José <jjrr2331@gmail.com>
2026-05-24 16:37:23 -0500

    Annotate TipoUsuarioWriteConverter with WritingConverter

--- src/main/java/com/icesi/uniplan/config/converter/TipoUsuarioWriteConverter.java
+++ src/main/java/com/icesi/uniplan/config/converter/TipoUsuarioWriteConverter.java
@@ -2,7 +2,9 @@ package com.icesi.uniplan.config.converter;
 
 import com.icesi.uniplan.model.mongo.enums.TipoUsuario;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.WritingConverter;
 
+@WritingConverter
 public class TipoUsuarioWriteConverter implements Converter<TipoUsuario, String> {
 
     @Override
