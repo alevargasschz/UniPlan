eb489eeb3d168f99a33f66ac7a252c223ca5e312
Juan José <jjrr2331@gmail.com>
2026-05-24 16:37:36 -0500

    Annotate TipoEventoWriteConverter with WritingConverter

--- src/main/java/com/icesi/uniplan/config/converter/TipoEventoWriteConverter.java
+++ src/main/java/com/icesi/uniplan/config/converter/TipoEventoWriteConverter.java
@@ -2,7 +2,9 @@ package com.icesi.uniplan.config.converter;
 
 import com.icesi.uniplan.model.mongo.enums.TipoEvento;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.WritingConverter;
 
+@WritingConverter
 public class TipoEventoWriteConverter implements Converter<TipoEvento, String> {
 
     @Override
